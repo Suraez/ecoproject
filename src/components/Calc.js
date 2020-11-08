4a27c2e8c0203368dda5b0d2db72d0880b2c1508
Suraez <oberais50@gmail.com>
2020-11-08 19:03:43 +0545

    needed to work on the route

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React  from 'react'
 
 import Footer from './Footer'
 import Navigation from './Navigation'
@@ -8,78 +8,99 @@ import PostComment from './PostComment'
 import assets from '../assets'
 import axios from "../axiosConfig"
 import AnsModal from './AnsModal'
+import Pagination from './UI/Pagination'
+import { Component } from 'react'
 
-function Calc(props) {
+class Calc extends Component{
+    state = {
+        commentArr: [],
+        currentPage: 1,
+        commentPerPage: 4
+    }
 
-    const [commentArr, setCommentArr] = useState(null);
-
-    useEffect(() => {
+    componentDidMount () {
+        console.log(this.props);
         axios.get('/comments.json')
-            .then(res => {
-                let allComments = []
-                for (let key in res.data) {
-                    allComments.push({
-                        id: key,
-                        ...res.data[key]
-                    })
-                }
-                setCommentArr(allComments)
-            })
-            .catch(err => console.log(err))
-    },[])
-    return (
-    <>
-        <div className="container bg-white shadow text-muted p-4">
-            <Navigation />
-            <div className="row d-flex justify-content-center m-2">
-                <div className="col-md-8 col-12 text-justify">
-                    Let this calculator do the heavy lifting.This formula helps you to calculate equivalent worth (Future Worth, F) given the values of Present Worth (P),
-                    interest rate per year (effective interest rate) and time (investment duration).
-                </div>
-            </div>
+        .then(res => {
+            let allComments = []
+            for (let key in res.data) {
+                allComments.push({
+                    id: key,
+                    ...res.data[key]
+                })
+            }
+            this.setState({commentArr: allComments})
+        })
+        .catch(err => console.log(err))
+    }
 
+    paginate = (page) => {
+        this.setState({currentPage: page})
+    }
+    
+    render() {
+        const updatedCommentArr = [...this.state.commentArr];
+        const indexOfLastComment = this.state.currentPage * this.state.commentPerPage;
+        const indexOfFirstComment = indexOfLastComment - this.state.commentPerPage;
+        const slicedCommentArr = updatedCommentArr.slice(indexOfFirstComment, indexOfLastComment);
 
-            <div className="row">
-                <div className="col-md-6 col-12">
-                    <img src={assets[props.location.state.imagePath]} alt="image1" className={styles.image}/>
-                </div>
-                <div className="col-md-6 col-12">
-                    <h4>Fill in the values.And get answers in seconds.</h4>
-                    <hr style={{background: '#55b8cf', height: '6px'}}/>
-                    <div className="form-group">
-                        <label htmlFor="firstInput">{props.location.state.sum} value</label>
-                        <input type="text"  id="firstInput" className="form-control" placeholder="e.g. 10000"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="secondInput">i value</label>
-                        <input type="text" id="secondInput" className="form-control" placeholder="e.g. type 0.1 for 10%"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="thirdInput">n value</label>
-                        <input type="text" id="thirdInput" className="form-control" placeholder="e.g. 5"/>
-                    </div>
-                    <div className="text-center">
-                        <AnsModal />
+        return (
+        <>
+            <div className="container bg-white shadow text-muted p-4">
+                <Navigation />
+                <div className="row d-flex justify-content-center m-2">
+                    <div className="col-md-8 col-12 text-justify">
+                        Let this calculator do the heavy lifting.This formula helps you to calculate equivalent worth (Future Worth, F) given the values of Present Worth (P),
+                        interest rate per year (effective interest rate) and time (investment duration).
                     </div>
                 </div>
+    
+    
+                <div className="row">
+                    <div className="col-md-6 col-12">
+                        <img src={assets[this.props.location.state.imagePath]} alt="image1" className={styles.image}/>
+                    </div>
+                    <div className="col-md-6 col-12">
+                        <h4>Fill in the values.And get answers in seconds.</h4>
+                        <hr style={{background: '#55b8cf', height: '6px'}}/>
+                        <div className="form-group">
+                            <label htmlFor="firstInput">{this.props.location.state.sum} value</label>
+                            <input type="text"  id="firstInput" className="form-control" placeholder="e.g. 10000"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="secondInput">i value</label>
+                            <input type="text" id="secondInput" className="form-control" placeholder="e.g. type 0.1 for 10%"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="thirdInput">n value</label>
+                            <input type="text" id="thirdInput" className="form-control" placeholder="e.g. 5"/>
+                        </div>
+                        <div className="text-center">
+                            <AnsModal />
+                        </div>
+                    </div>
+                </div>
+                <h4>Comments</h4>
+                <hr style={{background: '#55b8cf'}}/>
+    
+                <div className="row">
+                    <div className="col-md-6 col-12">
+                        {slicedCommentArr && slicedCommentArr.map(comment => <Comment  key={comment.id} date={comment.date} name={comment.name} comment={comment.comment}/>)} 
+                    </div>
+                    <div className="col-md-6 col-12">
+                        <PostComment />
+                    </div>
+                    <div className="col-md-6 col-12 mt-4 d-flex justify-content-center">
+                        <Pagination paginate={this.paginate}/>
+                    </div>
+                </div>
+                <hr style={{background: '#55b8cf'}}/>
+                <Footer />
+                
             </div>
-            <h4>Comments</h4>
-            <hr style={{background: '#55b8cf'}}/>
-
-            <div className="row">
-                <div className="col-md-6 col-12">
-                    {commentArr && commentArr.map(comment => <Comment  key={comment.id} date={comment.date} name={comment.name} comment={comment.comment}/>)} 
-                </div>
-                <div className="col-md-6 col-12">
-                    <PostComment />
-                </div>
-            </div>
-            <hr style={{background: '#55b8cf'}}/>
-            <Footer />
             
-        </div>
-        
-    </>
-    )
+        </>
+        )
+    }
 }
 export default Calc;
---
+++ src/components/UI/Pagination.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Pagination(props) {
+  return (
+    <div>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <Link className="page-link" to="#!" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+              <span className="sr-only">Previous</span>
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to="#!">
+              1
+            </Link>
+          </li>
+          <li className="page-item" onClick={() => props.paginate(2)}>
+            <Link className="page-link" to="/calc">
+              2
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to="#!">
+              3
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to="#!" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+              <span className="sr-only">Next</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
